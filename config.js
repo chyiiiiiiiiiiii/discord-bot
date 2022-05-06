4dc8e62ee3e3ac3a5a61c604d31aa1935adebac0
Yii <yii.chen@sensestar.live>
2022-05-06 22:10:48 +0800

    update packages

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 var _a, _b, _c, _d, _e, _f, _g, _h;
 exports.__esModule = true;
 exports.VERIFY_CHANNEL_ID = exports.LOG_CHANNEL_ID = exports.BOT_OWNER_ID = exports.BOT_TOKEN = void 0;
-exports.BOT_TOKEN = (_b = (_a = process === null || process === void 0 ? void 0 : process.env) === null || _a === void 0 ? void 0 : _a.BOT_TOKEN) !== null && _b !== void 0 ? _b : "OTY2OTE1MjM3MDY2NDY1Mjkw.YmIr_A.dcZCVCN3I58WDQ76iWZ8idI6PJQ";
+exports.BOT_TOKEN = (_b = (_a = process === null || process === void 0 ? void 0 : process.env) === null || _a === void 0 ? void 0 : _a.BOT_TOKEN) !== null && _b !== void 0 ? _b : "OTY2OTE1MjM3MDY2NDY1Mjkw.YmIr_A.dcZCVCN3I58WDQ76iWZ8i56HEX7";
 exports.BOT_OWNER_ID = (_d = (_c = process === null || process === void 0 ? void 0 : process.env) === null || _c === void 0 ? void 0 : _c.BOT_OWNER_ID) !== null && _d !== void 0 ? _d : "196509271238639618";
 exports.LOG_CHANNEL_ID = (_f = (_e = process === null || process === void 0 ? void 0 : process.env) === null || _e === void 0 ? void 0 : _e.LOG_CHANNEL_ID) !== null && _f !== void 0 ? _f : "966921902536933406";
 exports.VERIFY_CHANNEL_ID = (_h = (_g = process === null || process === void 0 ? void 0 : process.env) === null || _g === void 0 ? void 0 : _g.VERIFY_CHANNEL_ID) !== null && _h !== void 0 ? _h : "968025785854210048";
